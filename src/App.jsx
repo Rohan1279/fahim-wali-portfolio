8ea2ee7617997a4aafc9529e16eede8415b1c07a
Munir Uddin Rohan <muniruddinrohan@gmail.com>
2024-09-28 19:00:42 +0600

    Saas carousal done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Hero from "./components/Hero/Hero";
 import Inspiration from "./components/Inspiration/Inspiration";
 import Philosophy from "./components/Philosophy/Philosophy";
 import Showcase from "./components/Showcase/Showcase";
-
 const App = () => {
   return (
     <div className="bg-[#161414] min-h-screen flex flex-col justify-center items-center">
