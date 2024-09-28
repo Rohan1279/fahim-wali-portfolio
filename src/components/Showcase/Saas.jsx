27caf50de2004b4938d6b48f1975f05862734c5e
Munir Uddin Rohan <muniruddinrohan@gmail.com>
2024-09-28 20:57:19 +0600

    add LinearBlur component and enhance showcase styles

--- src/components/Showcase/Saas.jsx
+++ src/components/Showcase/Saas.jsx
@@ -1,12 +1,23 @@
 import { useCallback, useState, useRef } from "react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectFade,
+  EffectFlip,
+  EffectCube,
+} from "swiper/modules";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import ShowcaseCard from "./ShowcaseCard";
+import LinearBlur from "../ProgressiveBlur/ProgressiveBlur";
 
 const Saas = () => {
   const swiperRef = useRef(null);
+  const swiper = useSwiper();
   const htmlFor = "saas";
   const [isChecked, setIsChecked] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleOnClick = useCallback(
     (e) => {
@@ -20,11 +31,14 @@ const Saas = () => {
     [isChecked]
   );
 
-  const handleCloseClick = useCallback((e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setIsChecked(false);
-  }, []);
+  const handleCloseClick = useCallback(
+    (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsChecked(false);
+    },
+    []
+  );
 
   return (
     <ShowcaseCard
@@ -34,117 +48,100 @@ const Saas = () => {
       handleOnClick={handleOnClick}
       className={`relative`}
     >
-      <p
-        className={`opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500 ease-in-out delay-150 font-poppins text-xl text-[#161414] tracking-widest mt-[126px] ml-[57px] font-medium`}
-      >
-        product category
-      </p>
-      <h2 className="font-poppins text-[80px] font-extrabold text-[#2d2d2d] leading-[90%] peer-checked:text-[#2d2d2d]/10 transition-all duration-300 ease-in-out delay-150 ml-[54px]">
-        SaaS
-      </h2>
-      <div className="mt-[78px] ml-[57px] opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500 ease-in-out">
-        <p className="font-poppins font-medium text-xl text-[#161414] tracking-widest">
-          product name
+      <>
+        <p
+          className={`opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500 ease-in-out delay-150 font-poppins text-xl text-[#161414] tracking-widest mt-[126px] ml-[57px] font-medium`}
+        >
+          product category
         </p>
-        <h3 className="font-poppins font-extrabold text-[48px] text-[#2D2D2D]">
-          Driip
-        </h3>
-        <p className="font-poppins font-medium text-xl text-[#161414] tracking-widest leading-[3.7rem] mt-5">
-          role
-        </p>
-        <h3 className="font-poppins font-extralight text-[32px] text-[#2d2d2d] leading-[6px]">
-          product
-        </h3>
-        <h3 className="font-poppins font-extralight text-[32px] text-[#2d2d2d] ">
-          designer
-        </h3>
-      </div>{" "}
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        className="w-[745px] h-full absolute right-0 top-0 z-40 opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500 ease-in-out delay-1000"
-      >
-        <SwiperSlide className="flex flex-col border-red-400 pr-[64px] relative">
-          <img
-            src="/images/saas/slide-1.png"
-            alt="slide-1"
-            className="mt-auto  border-green-500"
-          />
-          <button className="" onClick={() => swiperRef.current.slideNext()}>
+        <h2 className="font-poppins text-[80px] font-extrabold text-[#2d2d2d] leading-[90%] peer-checked:text-[#2d2d2d]/10 transition-all duration-300 ease-in-out delay-150 ml-[54px]">
+          SaaS
+        </h2>
+        <div className="mt-[78px] ml-[57px] opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500 ease-in-out">
+          <p className="font-poppins font-medium text-xl text-[#161414] tracking-widest">
+            product name
+          </p>
+          <h3 className="font-poppins font-extrabold text-[48px] text-[#2D2D2D]">
+            Driip
+          </h3>
+          <p className="font-poppins font-medium text-xl text-[#161414] tracking-widest leading-[3.7rem] mt-5">
+            role
+          </p>
+          <h3 className="font-poppins font-extralight text-[32px] text-[#2d2d2d] leading-[6px]">
+            product
+          </h3>
+          <h3 className="font-poppins font-extralight text-[32px] text-[#2d2d2d] ">
+            designer
+          </h3>
+        </div>{" "}
+      </>
+      <div className="w-[745px] h-full absolute right-0 top-0 z-40 opacity-0 blur-sm peer-checked:blur-none peer-checked:opacity-100 transition-all duration-500">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          effect=""
+          speed={500}
+          modules={[]}
+          onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          className={`w-full h-full relative`}
+        >
+          {/*  ${isChecked ? "opacity-100" : "opacity-0"} */}
+          <SwiperSlide className="flex flex-col pr-[64px] relative">
             <img
-              src="/images/carousal-forward.png"
-              alt="carousal-forward"
-              className="absolute bottom-[21px] right-[86px] size-[50px]"
+              src="/images/saas/slide-1.png"
+              alt="slide-1"
+              className="mt-auto "
             />
-          </button>
-        </SwiperSlide>
-        <SwiperSlide className="flex">
-          <img
-            src="/images/saas/slide-2.png"
-            alt="slide-2"
-            className="my-auto w-[652px]"
-          />
+          </SwiperSlide>
+          <SwiperSlide className="flex">
+            <img
+              src="/images/saas/slide-2.png"
+              alt="slide-2"
+              className="my-auto w-[652px]"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex">
+            <img
+              src="/images/saas/slide-3.png"
+              alt="slide-3"
+              className="my-auto w-[652px]"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center pr-[64px]">
+            {" "}
+            <img
+              src="/images/saas/slide-4.png"
+              alt="slide-4"
+              className="my-auto w-[423px]"
+            />
+          </SwiperSlide>
+
           <button className="" onClick={() => swiperRef.current.slidePrev()}>
             <img
               src="/images/carousal-backward.png"
               alt="carousal-backward"
-              className="absolute bottom-[21px] left-0 size-[50px]"
+              className={`absolute bottom-[21px] left-0 size-[50px] z-50 ${
+                currentIndex !== 0
+                  ? "opacity-100 scale-100"
+                  : "opacity-0 scale-0"
+              } transition-all duration-[350] ease-in-out`}
             />
           </button>
+
           <button className="" onClick={() => swiperRef.current.slideNext()}>
             <img
               src="/images/carousal-forward.png"
               alt="carousal-forward"
-              className="absolute bottom-[21px] right-[86px] size-[50px]"
+              className={`absolute bottom-[21px] right-[86px] size-[50px] z-50  ${
+                currentIndex !== 3
+                  ? "opacity-100 scale-100"
+                  : "opacity-0 scale-0"
+              } transition-all duration-[350] ease-in-out`}
             />
           </button>
-        </SwiperSlide>
-        <SwiperSlide className="flex">
-          <img
-            src="/images/saas/slide-3.png"
-            alt="slide-3"
-            className="my-auto w-[652px]"
-          />
-          <button className="" onClick={() => swiperRef.current.slidePrev()}>
-            <img
-              src="/images/carousal-backward.png"
-              alt="carousal-backward"
-              className="absolute bottom-[21px] left-0 size-[50px]"
-            />
-          </button>
-          <button className="" onClick={() => swiperRef.current.slideNext()}>
-            <img
-              src="/images/carousal-forward.png"
-              alt="carousal-forward"
-              className="absolute bottom-[21px] right-[86px] size-[50px]"
-            />
-          </button>
-        </SwiperSlide>
-        <SwiperSlide className="flex border justify-center pr-[64px]">
-          {" "}
-          <img
-            src="/images/saas/slide-4.png"
-            alt="slide-4"
-            className="my-auto w-[423px]"
-          />
-          <button className="" onClick={() => swiperRef.current.slidePrev()}>
-            <img
-              src="/images/carousal-backward.png"
-              alt="carousal-backward"
-              className="absolute bottom-[21px] left-0 size-[50px]"
-            />
-          </button>
-          <button className="" onClick={() => swiperRef.current.slideNext()}>
-            <img
-              src="/images/carousal-forward.png"
-              alt="carousal-forward"
-              className="absolute bottom-[21px] right-[86px] size-[50px]"
-            />
-          </button>
-        </SwiperSlide>
-      </Swiper>
+        </Swiper>
+      </div>
       {isChecked && (
         <button
           onClick={handleCloseClick}
--- src/components/Showcase/ShowcaseCard.jsx
+++ src/components/Showcase/ShowcaseCard.jsx
@@ -12,7 +12,7 @@ const ShowcaseCard = ({
     <label
       htmlFor={htmlFor}
       onClick={handleOnClick}
-      className={`${className} min-w-[268px] h-[555px] rounded-[10px] bg-[#d9d9d9] select-none`}
+      className={`${className} min-w-[268px] h-[555px] rounded-[10px] bg-[#d9d9d9] select-none z-10`}
       {...rest}
     >
       <input
