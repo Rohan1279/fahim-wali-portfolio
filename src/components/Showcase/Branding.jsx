2a0b76143030f608c6a6e6aea9f85c1951ec3742
Munir Uddin Rohan <muniruddinrohan@gmail.com>
2024-09-30 22:37:13 +0600

    refactor Branding component styles for consistency

--- src/components/Showcase/Branding.jsx
+++ src/components/Showcase/Branding.jsx
@@ -6,12 +6,12 @@ const Branding = () => {
     <ShowcaseCard
       htmlFor={"branding"}
       id={"branding"}
-      className={`translate-y-[38px] transition-all ease-in-out duration-300 relative overflow-hidden group/branding pl-[35px] `}
+      className={`translate-y-[38px] transition-all ease-in-out duration-300 relative overflow-hidden group/branding `}
     >
       <img
         src="/images/branding-image.png"
         alt="branding-image"
-        className="w-[794px] mt-[32px] object-cover  opacity-0 group-has-[:checked]:opacity-100 transition-all duration-500 ease-in-out"
+        className="w-[794px] mt-[32px] ml-[35px]  object-cover opacity-0 group-has-[:checked]:opacity-100 transition-all duration-500 ease-in-out "
       />
       <div className="absolute top-[100px] right-[80px] transition-all delay-[600ms] z-50  font-poppins text-[128px] font-extrabold text-[#2d2d2d] leading-[5.2rem]">
         <span className="block">br</span>
